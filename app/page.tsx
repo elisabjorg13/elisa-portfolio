f06cf47371f63e736e65e33e398e9e27c44b0ead
Elisa Bjorg <davidsson.elisa@gmail.com>
2025-08-29 13:28:17 +0000

    compressed hdri

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,6 @@ import Scene from "./components/Scene";
 export default function Home() {
   return (
     <div>
-
-
-    <div className="w-full">
-      <h1>hello</h1>
-    </div>
     <div className="flex items-center justify-center min-h-screen">
       <Scene />
     </div>
